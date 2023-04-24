4c692f4a7ab4245069af4386559032b12a67854f
omabelle73 <chiderauchendu4@gmail.com>
2023-04-24 11:42:20 -0700

    task 11

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,36 +1,27 @@
 #include <stdio.h>
+
 /**
- *main-program entry point.
- *Return:0 success, non zero fail
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
-	int i, k = '0', j;
-
-	for (i = '0'; i <= '9'; i++)
+	int n, m;
+for (n = 48; n <= 56; n++)
 	{
-		for (j = k; j <= '9'; j++)
+		for (m = 49; m <= 57; m++)
 		{
-if (i != j)
+			if (m > n)
 			{
-				putchar(i);
-				putchar(j);
-			}
-			if (i == j)
-			{
-				continue;
-			}
-			if (i == '8' && j == '9')
-			{
-				break;
-			}
-			else
-			{
-				putchar(',');
-				putchar(' ');
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
-		k++;
 	}
 	putchar('\n');
 	return (0);
